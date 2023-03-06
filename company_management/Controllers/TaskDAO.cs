90b52f0115478f73c3c80d4c04354e00a8f61a20
chhinhua <chhinhua@gmail.com>
2023-03-06 22:25:01 +0700

    CRUD: Kpi, User, Task

---
+++ company_management/Controllers/TaskDAO.cs
@@ -0,0 +1,36 @@
+﻿using company_management.Models;
+using System.Windows.Forms;
+
+namespace company_management.Controllers
+{
+    public class TaskDAO
+    {
+        private readonly DBConnection dBConnection;
+
+        public TaskDAO() => dBConnection = new DBConnection();
+
+        public void loadTask(DataGridView dataGridView) => dBConnection.loadData(dataGridView, "task");
+
+        public void addTask(Task task)
+        {                                       
+            string sqlStr = string.Format("INSERT INTO task(idUser, taskName, description, deadline, progress)" +
+                   "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')",
+                   task.IdUser, task.TaskName, task.Description, task.Deadline, task.Progress);
+            dBConnection.executeQuery(sqlStr);
+        }
+
+        public void updateTask(Task task)
+        {
+            string sqlStr = string.Format("UPDATE task SET " +
+                   "idUser = '{0}', taskName = '{1}', description = '{2}', deadline = '{3}', progress = '{4}' WHERE id = '{5}'",
+                   task.IdUser, task.TaskName, task.Description, task.Deadline, task.Progress, task.IdTask);
+            dBConnection.executeQuery(sqlStr);
+        }
+
+        public void deleteTask(int id)
+        {
+            string sqlStr = string.Format("DELETE FROM task WHERE id = '{0}'", id);
+            dBConnection.executeQuery(sqlStr);
+        }
+    }
+}
---
+++ company_management/DBConnection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace company_management
+{
+    public class DBConnection
+    {
+        private readonly SqlConnection connection;
+
+        public DBConnection() => connection = new SqlConnection();
+
+        public void loadData(DataGridView dataGridView, string tableName)
+        {
+            try
+            {
+                connection.Open();
+                string sqlStr = string.Format("SELECT * FROM {0}", tableName);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlStr, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridView.DataSource = dataTable;
+                dataGridView.Columns["Id"].Visible = false;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void executeQuery(string sqlStr)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sqlStr, connection);
+
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("Successfully");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed action");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
+
