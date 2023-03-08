a4cebf9751598d58ffc46ac29290f9de79ea7ae8
chhinhua <chhinhua@gmail.com>
2023-03-08 14:59:14 +0700

    update CRUD: Salary, LeaveRq, CiCo

--- company_management/Controllers/CheckinCheckoutDAO.cs
+++ company_management/Controllers/CheckinCheckoutDAO.cs
@@ -9,18 +9,19 @@ namespace company_management.Controllers
 
         public CheckinCheckoutDAO() => dBConnection = new DBConnection();
 
-        public void loadCheckinCheckout(DataGridView dataGridView) => dBConnection.loadData(dataGridView, "checkinCheckout");
+        public void loadCheckinCheckout(DataGridView dataGridView) => dBConnection.loadData(dataGridView, "checkin_checkout");
 
         public void addCheckinCheckout(CheckinCheckout checkinCheckout)
         {
-            string sqlStr = string.Format("INSERT INTO checkinCheckout(idUser, checkinTime, checkoutTime, totalHours, date)" +
-                "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')", checkinCheckout.IdUser, checkinCheckout.CheckinTime, checkinCheckout.CheckoutTime, checkinCheckout.TotalHours, checkinCheckout.Date);
+            string sqlStr = string.Format("INSERT INTO checkin_checkout(idUser, checkinTime, checkoutTime, totalHours, date)" +
+                "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')", 
+                checkinCheckout.IdUser, checkinCheckout.CheckinTime, checkinCheckout.CheckoutTime, checkinCheckout.TotalHours, checkinCheckout.Date);
             dBConnection.executeQuery(sqlStr);
         }
 
         public void updateCheckinCheckout(CheckinCheckout checkinCheckout)
         {
-            string sqlStr = string.Format("UPDATE CheckinCheckout SET " +
+            string sqlStr = string.Format("UPDATE checkin_checkout SET " +
                    "idUser = '{0}', checkinTime = '{1}', checkoutTime = '{2}', totalHours = '{3}', date = '{4}' WHERE id = '{5}'",
                    checkinCheckout.IdUser, checkinCheckout.CheckinTime, checkinCheckout.CheckoutTime, checkinCheckout.TotalHours, checkinCheckout.Date, checkinCheckout.IdCheckin);
             dBConnection.executeQuery(sqlStr);
@@ -28,7 +29,7 @@ namespace company_management.Controllers
 
         public void deleteCheckinCheckout(int id)
         {
-            string sqlStr = string.Format("DELETE FROM CheckinCheckout WHERE id = '{0}'", id);
+            string sqlStr = string.Format("DELETE FROM checkin_checkout WHERE id = '{0}'", id);
             dBConnection.executeQuery(sqlStr);
         }
     }
--- company_management/Controllers/LeaveRequestDAO.cs
+++ company_management/Controllers/LeaveRequestDAO.cs
@@ -9,26 +9,27 @@ namespace company_management.Controllers
 
         public LeaveRequestDAO() => dBConnection = new DBConnection();
 
-        public void loadLeaveRequest(DataGridView dataGridView) => dBConnection.loadData(dataGridView, "leaveRequest");
+        public void loadLeaveRequest(DataGridView dataGridView) => dBConnection.loadData(dataGridView, "leave_request");
 
         public void addLeaveRequest(LeaveRequest leaveRequest)
         {
-            string sqlStr = string.Format("INSERT INTO leaveRequest(startDate, endDate, numberDay, reason, status)" +
-                "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')", leaveRequest.StartDate, leaveRequest.EndDate, leaveRequest.NumberDay, leaveRequest.Reason, leaveRequest.Status);
+            string sqlStr = string.Format("INSERT INTO leave_request(idUser, startDate, endDate, numberDay, reason, status)" +
+                "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')", 
+                leaveRequest.IdUser, leaveRequest.StartDate, leaveRequest.EndDate, leaveRequest.NumberDay, leaveRequest.Reason, leaveRequest.Status);
             dBConnection.executeQuery(sqlStr);
         }
 
         public void updateLeaveRequest(LeaveRequest leaveRequest)
         {
-            string sqlStr = string.Format("UPDATE LeaveRequest SET " +
-                   "startDate = '{0}', endDate = '{1}', numberDay = '{2}', reason = '{3}', status = '{4}' WHERE id = '{5}'",
-                   leaveRequest.StartDate, leaveRequest.EndDate, leaveRequest.NumberDay, leaveRequest.Reason, leaveRequest.Status, leaveRequest.IdUser);
+            string sqlStr = string.Format("UPDATE leave_request SET " +
+                   "idUser = '{0}', startDate = '{1}', endDate = '{2}', numberDay = '{3}', reason = '{4}', status = '{5}' WHERE id = '{6}'",
+                   leaveRequest.IdUser, leaveRequest.StartDate, leaveRequest.EndDate, leaveRequest.NumberDay, leaveRequest.Reason, leaveRequest.Status, leaveRequest.IdUser);
             dBConnection.executeQuery(sqlStr);
         }
 
         public void deleteLeaveRequest(int id)
         {
-            string sqlStr = string.Format("DELETE FROM LeaveRequest WHERE id = '{0}'", id);
+            string sqlStr = string.Format("DELETE FROM leave_request WHERE id = '{0}'", id);
             dBConnection.executeQuery(sqlStr);
         }
     }
